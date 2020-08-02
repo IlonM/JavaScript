9e536af4994c18d2cc89cfe9a29a8753ad021bc8
Pavel <PaulPetrenko@mail.ru>
2020-08-02 19:24:20 +0300

    Second commit

---
+++ Lesson 7/script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// console.log(4 / 0);
+// console.log('string' * 2);
+// let somesing;
+// console.log(somesing);
+
+// let persone = {
+//     name: "John",
+//     age: 26,
+//     isMarried: false
+// };
+
+// console.log(persone["age"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+// console.log(arr[2]);
+
+// var age = 25;
+// var message = 'Привет!';
+// var user = "admin";
+
+// console.log(message);
+
+// var age = 25,
+//     message = 'Я устал',
+//     user = "admin";
+
+// console.log(typeof(arr));
+
+// //  alert("Hello World!");
+// // let answer = confirm("Are you here?");
+
+// // console.log(answer);
+
+// let answer = +prompt("Ты красавчик?", "Да");
+
+// console.log(typeof(answer));
+
+// // console.log("arr" + " - object");
+// console.log(4 + +"-object");
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(incr);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose   = false;
+
+console.log(isChecked || !isClose);
